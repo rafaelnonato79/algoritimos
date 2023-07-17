174749724f5356f1dd962999d9c790d5f35a37da
Rafael Nonato <rafaelnonato79@gmail.com>
2023-07-17 19:52:15 -0300

    Orientação a objetos

---
+++ javascript/OrientacaoAobjetos/carros.js
@@ -0,0 +1,18 @@
+export class Carro{
+    //atributos
+    marca;
+    modelo;
+    ano;
+
+    // construtor 
+    constructor(modelo,marca,ano){
+        this.modelo;
+        this.marca;
+        this.ano;
+    }
+    // metodos
+
+}
+
+
+
---
+++ javascript/OrientacaoAobjetos/main.js
@@ -0,0 +1,9 @@
+import { Carro } from 'Carro';
+// instanciando o objeto up
+const up = new Carro(`Up`,`VW`,2015);
+
+// instanciando o objeto ka
+const ka = new Carro(`Ka`,`Ford`,2006);
+
+
+console(ka.marca);
